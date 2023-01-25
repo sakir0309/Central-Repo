41b7b6519fbf2adf9c016ca17fce6b056fc705a5
sakir <sakirgizali@gmail.com>
2023-01-25 17:37:10 +0530

    First file

---
+++ main.py
@@ -0,0 +1,17 @@
+num1=int(input("enter the number 1:-"))
+
+num2=int(input("enter the number 2:-"))
+
+arg=input("enter the value:-")
+
+if arg=="+":
+    print(int(num1+num2))
+elif arg=="-":
+    print(int(num1-num2))
+elif arg=="*":
+    print(int(num1*num2))
+elif arg=="/":
+    print(int(num1/num2))
+else:
+    print("invalid argunment")
+
